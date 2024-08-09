19ac47a09f7399dc04917329b10ea4207226bac5
tomit4 <mosssap@gmail.com>
2024-08-08 22:35:59 -0700

    :wheelchair: Added meta for seo/accessibility

--- src/views/stylers.rs
+++ src/views/stylers.rs
@@ -8,7 +8,7 @@ pub fn Stylers() -> impl IntoView {
     view! {
         <div class=style::stylers>
             <h1 class=style::red id="two">"Hello"</h1>
-            <h2 class=style::green >"World"</h2>
+            <h2 class=style::green>"World"</h2>
             <h2>"Hi There"</h2>
             <h3>"Hello Kanna"</h3>
         </div>
